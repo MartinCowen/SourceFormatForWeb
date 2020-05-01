d2eea352abbcad08455f6f48bbe50b8eb4c830c4
Martin Cowen <work@martincowen.me.uk>
2020-05-01 15:10:50 +0100

    Designed form. Added list of langs, format function.

--- SourceFormatForWeb/Form1.Designer.vb
+++ SourceFormatForWeb/Form1.Designer.vb
@@ -22,10 +22,135 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        Me.txtInput = New System.Windows.Forms.TextBox()
+        Me.txtOutput = New System.Windows.Forms.TextBox()
+        Me.cmbLangs = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.chkAutoDetectLang = New System.Windows.Forms.CheckBox()
+        Me.btnCopy = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Panel1.Controls.Add(Me.SplitContainer1)
+        Me.Panel1.Location = New System.Drawing.Point(2, 58)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(793, 495)
+        Me.Panel1.TabIndex = 0
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.Controls.Add(Me.txtInput)
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.Controls.Add(Me.txtOutput)
+        Me.SplitContainer1.Size = New System.Drawing.Size(793, 495)
+        Me.SplitContainer1.SplitterDistance = 432
+        Me.SplitContainer1.TabIndex = 0
+        '
+        'txtInput
+        '
+        Me.txtInput.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.txtInput.Font = New System.Drawing.Font("Lucida Console", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtInput.Location = New System.Drawing.Point(0, 0)
+        Me.txtInput.Multiline = True
+        Me.txtInput.Name = "txtInput"
+        Me.txtInput.Size = New System.Drawing.Size(432, 495)
+        Me.txtInput.TabIndex = 0
+        '
+        'txtOutput
+        '
+        Me.txtOutput.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.txtOutput.Font = New System.Drawing.Font("Lucida Console", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtOutput.Location = New System.Drawing.Point(0, 0)
+        Me.txtOutput.Multiline = True
+        Me.txtOutput.Name = "txtOutput"
+        Me.txtOutput.Size = New System.Drawing.Size(357, 495)
+        Me.txtOutput.TabIndex = 0
+        '
+        'cmbLangs
+        '
+        Me.cmbLangs.FormattingEnabled = True
+        Me.cmbLangs.Location = New System.Drawing.Point(74, 21)
+        Me.cmbLangs.Name = "cmbLangs"
+        Me.cmbLangs.Size = New System.Drawing.Size(150, 21)
+        Me.cmbLangs.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 24)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(55, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Language"
+        '
+        'chkAutoDetectLang
+        '
+        Me.chkAutoDetectLang.AutoSize = True
+        Me.chkAutoDetectLang.Location = New System.Drawing.Point(247, 24)
+        Me.chkAutoDetectLang.Name = "chkAutoDetectLang"
+        Me.chkAutoDetectLang.Size = New System.Drawing.Size(81, 17)
+        Me.chkAutoDetectLang.TabIndex = 3
+        Me.chkAutoDetectLang.Text = "Auto detect"
+        Me.chkAutoDetectLang.UseVisualStyleBackColor = True
+        '
+        'btnCopy
+        '
+        Me.btnCopy.Location = New System.Drawing.Point(682, 21)
+        Me.btnCopy.Name = "btnCopy"
+        Me.btnCopy.Size = New System.Drawing.Size(110, 29)
+        Me.btnCopy.TabIndex = 4
+        Me.btnCopy.Text = "Copy"
+        Me.btnCopy.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(800, 555)
+        Me.Controls.Add(Me.btnCopy)
+        Me.Controls.Add(Me.chkAutoDetectLang)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.cmbLangs)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel1.PerformLayout()
+        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        Me.SplitContainer1.Panel2.PerformLayout()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents SplitContainer1 As SplitContainer
+    Friend WithEvents txtInput As TextBox
+    Friend WithEvents txtOutput As TextBox
+    Friend WithEvents cmbLangs As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents chkAutoDetectLang As CheckBox
+    Friend WithEvents btnCopy As Button
 End Class
--- SourceFormatForWeb/Form1.vb
+++ SourceFormatForWeb/Form1.vb
@@ -1,3 +1,36 @@
 ﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        PopulateLangs()
+    End Sub
+    Private Sub PopulateLangs()
+        With cmbLangs
+            .Enabled = False
+            For Each s As String In My.Settings.Language
+                cmbLangs.Items.Add(s)
+            Next
+            .Enabled = True
 
+        End With
+    End Sub
+
+    Private Function Convert(s As String, lang As String) As String
+        Dim r As String
+        'replace tabs with 4 spaces
+        r = s.Replace(vbTab, "    ")
+
+        r = r.Insert(0, "<code class=""language-" & lang & """>")
+
+        r = r.TrimEnd(vbCrLf)
+        r = r & "</code>"
+        Return r
+    End Function
+
+    Private Sub txtInput_TextChanged(sender As Object, e As EventArgs) Handles txtInput.TextChanged
+
+        txtOutput.Text = Convert(txtInput.Text, cmbLangs.Text)
+    End Sub
+
+    Private Sub cmbLangs_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbLangs.SelectedIndexChanged
+        txtOutput.Text = Convert(txtInput.Text, cmbLangs.Text)
+    End Sub
 End Class
